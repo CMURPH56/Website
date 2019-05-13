0c41a8000a1b27e428d20e5bea7830c75a57db34
Cam Murphy <CamMurphy195@gmail.com>
2019-05-12 22:10:13 -0500

    updated about me content

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,55 @@
   <section>
     <Nav />
     <div class='content'>
-      <h1> Test </h1>
+
+      <h1> About Me</h1>
+      <p>
+        Originaly from a small town in Southern New Hampshire. 
+        I moved to Chicago to study at Depaul University.
+      </p>
+      <img src="./cam.jpg" />
+      <h1> College </h1>
+      <p>
+        I was initially a Physics Major. However, After a few years in the Physics program. I realized, I wanted 
+        something more practical. I switched to Computer Science Sophmore Year, and after
+        I took the  the Python introduction classes
+        there was no turning back. The further I got into large scale software, Java,
+        and design patterns the more I enjoyed it. I graduated in 2018 with my Degree in Software Development and 
+        a minor in Physics.
+      </p>
+      <h1> Internship </h1>
+      <p>
+        In the summer of 2018, I got my first
+        internship at Pronto Progress. It was a small company that did
+        consulting work with companies who use Epicor. I was responsible 
+        for writing small programs in C# that would modify the existing software.
+      </p>
+      <h1> Career </h1>
+      <p>
+        After the summer, I got another offer from One North Interactive
+        for an internship. I worked part time at One North and finished up 
+        my degree at DePaul University. After graduating, I accepted a full time 
+        position as an Associate Software Developer. Where I work on large scale .NET application
+        with the Sitecore CMS, and smaller scale JavaScript based WordPress and Keystone sites.
+      </p>
+      <h1> Mentorship  </h1>
+      <p>
+        In the Fall of 2018 I applied to be a mentor with Chicago Python Users Group.
+        The program takes someone who is more experienced in programming and pairs them 
+        with someone who wants to learn more in a project based environment. The project
+        I worked on was connected a Raspberry Pi to a cars OBDII port to get real time 
+        data sent to a React Website hosted on Heroku. We went on to win the competition that
+        season.
+      </p>
+      <h1> Side Projects </h1>
+      <p>
+        I try to be
+        a frequent Contributer to github, and have started a few side projects.
+        Including most recently
+        <a href='http://www.Camshorrorquiz.com'> CamsHorrorQuiz.com</a>.
+        I also like to work on smaller home projects with Raspberry Pi's
+        or Arduinos. 
+      </p>
     </div>
   </section>
 </template>
@@ -17,6 +65,23 @@
 </script>
 <style>
 .content{
-  border-top: 2px solid purple;
+  width: 45%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.content h1 {
+  color: purple;
+}
+img {
+  height: auto;
+  width:  350px;
+}
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+  .content {
+    width: 100%;
+  }
+  img{
+    width: 100%;
+  }
 }
 </style>
