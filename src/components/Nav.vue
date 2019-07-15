f9247b771029fc8b13a49bdab29656b28e370781
Cam Murphy <CamMurphy195@gmail.com>
2019-07-14 20:28:33 -0500

    worked on better way to code up hamburger menu

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,4 +1,5 @@
 <template> 
+<section>
   <div id="navbar" class="navbar">
     <ul>
       <div class="name">
@@ -17,197 +18,62 @@
         <a target="_blank" href="./resume.pdf">Resume</a>
       </div>
     </ul>
-
-    <!--    Made by Erik Terwan    -->
-<!--   24th of November 2015   -->
-<!--        MIT License        -->
-<nav role="navigation">
-  <div id="menuToggle">
-    <!--
-    A fake / hidden checkbox is used as click reciever,
-    so you can use the :checked selector on it.
-    -->
-    <input type="checkbox" />
-    
-    <!--
-    Some spans to act as a hamburger.
-    
-    They are acting like a real hamburger,
-    not that McDonalds stuff.
-    -->
-    <span></span>
-    <span></span>
-    <span></span>
-    
-    <!--
-    Too bad the menu has to be inside of the button
-    but hey, it's pure CSS magic.
-    -->
-    <ul id="menu">
-      <a href="#"><li>Home</li></a>
-      <a href="#"><li>About</li></a>
-      <a href="#"><li>Info</li></a>
-      <a href="#"><li>Contact</li></a>
-      <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
-    </ul>
+    </div>
+    <div class="hamburger">
+      <label for="toggle">&#9776;</label>
+      <input type="checkbox" id="toggle"/>
+      <div class="menu">
+        <a href="#">test</a>
+        <a href="#">test</a>
+        <a href="#">test</a>
+      </div>
   </div>
-</nav>
-
-  </div>
+  </section>  
 </template>
 
 <style>
-body
-{
-  margin: 0;
-  padding: 0;
-  /* make it look decent enough */
-  background: #232323;
-  color: #cdcdcd;
-  font-family: "Avenir Next", "Avenir", sans-serif;
+.nav {
+  border-bottom: 1px solid #eaeaeb;
+  text-align: right;
+  height: 70px;
+  line-height: 70px;
 }
-
-a
-{
+.menu{
+  margin: 0 30px 0 0;
+}
+.menu a {
   text-decoration: none;
-  color: #232323;
-  
-  transition: color 0.3s ease;
+  color: grey;
+  margin: 0 10px;
+  line-height: 70px; 
 }
-
-a:hover
-{
-  color: rgb(248, 247, 247);
+label {
+  margin: 0 40px 0 0;
+  font-size: 26px;
+  line-height: 70px; 
+  display: none; 
 }
-
-#menuToggle
-{
-  display: block;
-  position: relative;
-  top: 50px;
-  left: 50px;
-  
-  z-index: 1;
-  
-  -webkit-user-select: none;
-  user-select: none;
+#toggle {
+  display: none;  
 }
-
-#menuToggle input
-{
-  display: block;
-  width: 40px;
-  height: 32px;
-  position: absolute;
-  top: -7px;
-  left: -5px;
-  
-  cursor: pointer;
-  
-  /* opacity: 0; hide this */
-  z-index: 2; /* and place it over the hamburger */
-  
-  -webkit-touch-callout: none;
-}
-
-  /*
-  * Just a quick hamburger
-  */
-  #menuToggle span
-  {
+@media only screen and (max-width: 500px) {
+  label {
     display: block;
-    width: 33px;
-    height: 4px;
-    margin-bottom: 5px;
-    position: relative;
-    
-    background: #cdcdcd;
-    border-radius: 3px;
-    
-    z-index: 1;
-    
-    transform-origin: 4px 0px;
-    
-    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
-                background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
-                opacity 0.55s ease;
+    cursor: pointer;
   }
-
-  #menuToggle span:first-child
-  {
-    transform-origin: 0% 0%;
+  .menu {
+    text-align: center;
+    width: 100%;
+    display: none;
   }
-
-  #menuToggle span:nth-last-child(2)
-  {
-    transform-origin: 0% 100%;
+  .menu a {
+    display: block;
+    border-bottom: 1px solid #eaeaeb;
   }
-
-  /* 
-  * Transform all the slices of hamburger
-  * into a crossmark.
-  */
-  #menuToggle input:checked ~ span
-  {
-    opacity: 1;
-    transform: rotate(45deg) translate(-2px, -1px);
-    background: #232323;
+  #toggle:checked + .menu {
+    display:block;
   }
-
-  /*
-  * But let's hide the middle one.
-  */
-  #menuToggle input:checked ~ span:nth-last-child(3)
-  {
-    opacity: 0;
-    transform: rotate(0deg) scale(0.2, 0.2);
-  }
-
-  /*
-  * Ohyeah and the last one should go the other direction
-  */
-  #menuToggle input:checked ~ span:nth-last-child(2)
-  {
-    transform: rotate(-45deg) translate(0, -1px);
-  }
-
-  /*
-  * Make this absolute positioned
-  * at the top left of the screen
-  */
-  #menu
-  {
-    position: absolute;
-    width: 300px;
-    margin: -100px 0 0 -50px;
-    padding: 50px;
-    padding-top: 125px;
-    
-    background: #ededed;
-    list-style-type: none;
-    -webkit-font-smoothing: antialiased;
-    /* to stop flickering of text in safari */
-    
-    transform-origin: 0% 0%;
-    transform: translate(-100%, 0);
-    
-    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
-  }
-
-  #menu li
-  {
-    padding: 10px 0;
-    font-size: 22px;
-  }
-
-  /*
-  * And let's slide it in from the left
-  */
-  #menuToggle input:checked ~ ul
-  {
-    transform: none;
-  }
-
+}
 .navbar{
   position: sticky;
   position: -webkit-sticky;
